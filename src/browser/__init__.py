a95aee07cdac7f1a94dbd8591c84225e2fdd3961
Kevin Guzman <kevocde@gmail.com>
2023-02-13 07:21:24 -0500

    [added] version and descriptor

--- src/browser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .src.action_executor import ActionExecutor
-from .src.config import BrowserType, Config
-from .src.custom_web_driver_handler import CustomWebDriverHandler
-from .src.web_driver_facade import WebDriverFacade
