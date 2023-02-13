21fe389ebf6be79b8b9a2dc8db3797df04755c0f
Kevin Guzman <kevocde@gmail.com>
2023-02-12 21:22:53 -0500

    [added] package refactoring structure

--- src/browser/__init__.py
+++ src/browser/__init__.py
@@ -1,2 +1,4 @@
-from .config import Config
-from .web_driver_facade import WebDriverFacade
+from .src.action_executor import ActionExecutor
+from .src.config import BrowserType, Config
+from .src.custom_web_driver_handler import CustomWebDriverHandler
+from .src.web_driver_facade import WebDriverFacade
