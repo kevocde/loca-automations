8403e399f1f7ff85d533dd389fdb53765e7303f6
kevocde <kevocde@gmail.com>
2023-03-19 08:53:35 -0500

    [added] slack app validation

--- src/cloud/__init__.py
+++ src/cloud/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 server = Flask(__name__)
@@ -6,6 +6,11 @@ server = Flask(__name__)
 @server.route('/')
 def welcome_page():
   return 'Welcome to the Cloud Webhook by Kevocde'
+
+@server.route('/slack/validation', methods=['POST'])
+def validate_slack_app():
+  data = request.get_json()
+  return data['challenge']
 
 def create_app():
   return server
