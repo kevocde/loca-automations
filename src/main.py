327e7c02222c53b907fe9d91ce5016faf768fd72
kevocde <kevocde@gmail.com>
2023-03-19 08:24:23 -0500

    [added] new command

--- src/main.py
+++ src/main.py
@@ -16,5 +16,12 @@ def cloud_start(bind: str = typer.Argument("0.0.0.0:8000", envvar="CLOUD_BIND"))
     gunicorn_cmd = "gunicorn 'src.cloud:create_app()' -b {}".format(bind)
     subprocess.run(gunicorn_cmd, shell=True, check=True)
 
+@app.command('cloud:stop')
+def cloud_stop(service: str = typer.Argument("gunicorn", envvar="CLOUD_SERVICE")):
+    if service == "gunicorn":
+        subprocess.run("pkill gunicorn", shell=True, check=True)
+    else:
+        subprocess.run("systemctl stop {}".format(service), shell=True, check=True)
+
 if __name__ == '__main__':
     app()
